0e7933cc18f1600f6d662874ddfff6a5c021ff90
EggyDoctor <98979582+EggyDoctor@users.noreply.github.com>
2022-02-04 01:44:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 client.on("ready", async function() {
   console.log("ready")
   const gen = await client.channels.fetch("934029386838073357")
+  gen.send("ayo the pizza here")
 })
 
 client.login(process.env.TOKEN)
